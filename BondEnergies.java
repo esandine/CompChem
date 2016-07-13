ff627c8f4fa2001546b9b859504d30754ec1a9b5
esandine <esandine@stuy.edu>
2016-07-13 10:45:42 -0400

    Modified to use hess's law

--- BondEnergies.java
+++ BondEnergies.java
@@ -3,22 +3,24 @@ public class BondEnergies{
     private class Bond{
 	public String name;
 	public double BE;
-	public Bond(String s){
+	public Bond(String s, double o, double w, double c, double n){
 	    name = s;
 	    if(name.equals("CO")){//C0 double bond in CO2
-		BE=799;
+		BE=c/2;
 	    }else if(name.equals("NN")){//half of a NN bond in N2
-		BE=946/2;//Need to find value
+		BE=n/2;//Need to find value
 	    }else if(name.equals("HO")){//HO bond in H2O
-		BE=464.0+22.0080000;
+		BE=w/2+22.0080000;
 	    }else if(name.equals("OO")){//HALF of a bond in O2
-		BE=498/2;
+		BE=o/2;
 	    }else{
 		throw new IllegalArgumentException();
 	    }
 	}
     }
-    public BondEnergies(int[] data){
+
+
+    public BondEnergies(int[] data, double o, double w, double c, double n){
 	if(data.length!=4){
 	    throw new IllegalArgumentException();
 	}else{
@@ -26,9 +28,18 @@ public class BondEnergies{
 	    for(int i = 0; i < data.length; i++){
 		CHON[i]=data[i];
 	    }
+	    oxygen = o;
+	    water = w;
+	    carbondioxide = c;
+	    nitrogen = n;
 	}
     }
+    //Instance Variables
     private int[] CHON;
+    private double oxygen;
+    private double water;
+    private double carbondioxide;
+    private double nitrogen;
     private void doubleCoefficients(){//To deal with odd balancing situations and ensure H is always even
 	for(int i = 0; i < CHON.length; i++){
 	    CHON[i]*=2;
@@ -39,7 +50,7 @@ public class BondEnergies{
 	    CHON[i]/=2;
 	}
     }
-    public double eval(){
+    public double eval(double fuel){
 	boolean doubled = false;
 	ArrayList<Bond> products = new ArrayList<Bond>();
 	ArrayList<Bond> reactants = new ArrayList<Bond>();
@@ -48,32 +59,32 @@ public class BondEnergies{
 	    doubled = true;
 	}
 	for(int c = 0; c<CHON[0]; c++){
-	    products.add(new Bond("CO"));
-	    products.add(new Bond("CO"));
+	    products.add(new Bond("CO",oxygen,water,carbondioxide,nitrogen));
+	    products.add(new Bond("CO",oxygen,water,carbondioxide,nitrogen));
 	}
 	for(int n = 0; n<CHON[3];n++){
-	    products.add(new Bond("NN"));
+	    products.add(new Bond("NN",oxygen,water,carbondioxide,nitrogen));
 	}
 	for(int h = 0; h<CHON[1]; h++){
-	    products.add(new Bond("HO"));
+	    products.add(new Bond("HO",oxygen,water,carbondioxide,nitrogen));
 	}
 	int oxygens = -CHON[2];
 	oxygens+=2*CHON[0];
 	oxygens+=CHON[1]/2;
 	for(int o = 0; o<oxygens; o++){
-	    reactants.add(new Bond("OO"));
+	    reactants.add(new Bond("OO",oxygen,water,carbondioxide,nitrogen));
 	}
-	double retValue = 0;
+	double retValue = -fuel;
 	//System.out.println("Reactants");
 	for(Bond m : reactants){
-	    retValue+=m.BE;
+	    retValue-=m.BE;
 	    //System.out.println(m.name);
 	    //System.out.println(m.BE);
 	    //System.out.println(retValue);
 	}
 	//System.out.println("Products");
 	for(Bond m : products){
-	    retValue-=m.BE;
+	    retValue+=m.BE;
 	    //System.out.println(m.name);
 	    //System.out.println(m.BE);
 	    //System.out.println(retValue);
@@ -161,11 +172,22 @@ public class BondEnergies{
 	inputs[2]=Integer.parseInt(s1.next());
 	System.out.println("Enter the number of Nitrogens");
 	inputs[3]=Integer.parseInt(s1.next());
+	double[] chon = new double[5];
+	System.out.println("Enter the heat of formation of your fuel");
+	chon[0]=Double.parseDouble(s1.next());
+	System.out.println("Enter the heat of formation of oxygen");
+	chon[1]=Double.parseDouble(s1.next());
+	System.out.println("Enter the heat of formation of water");
+	chon[2]=Double.parseDouble(s1.next());
+	System.out.println("Enter the heat of formation of carbon dioxide");
+	chon[3]=Double.parseDouble(s1.next());
+	System.out.println("Enter the heat of formation of nitrogen");
+	chon[4]=Double.parseDouble(s1.next());
 	//for(int i = 0; i<inputs.length;i++){
 	//    inputs[i]=Integer.parseInt(args[i]);
 	//}
-	BondEnergies b = new BondEnergies(inputs);
-	System.out.println(b.eval());
+	BondEnergies b = new BondEnergies(inputs,chon[1],chon[2],chon[3],chon[4]);
+	System.out.println(b.eval(chon[0]));
     }
 
 }
